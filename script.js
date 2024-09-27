61909357b60431683393b5badccf7253e34fe8a2
Elisa Afonso <Oliveiralilly43@gmail.com>
2024-09-27 11:57:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(jogador1, jogador2) {
+    const opcoesValidas = ("Pedra", "Papel","Tesoura");
+
+ if(jogador1 === jogador2){
+    return "Empate!";
+} 
+
+else if(
+    (jogador1 === "Pedra" && jogador2 === "Tesoura") || 
+    (jogador1 === "Tesoura" && jogador2 === "Papel") ||
+    (jogador1 === "Papel" && jogador2 === "Pedra")
+    )
+    {
+        return "Jogador 1 venceu!";
+    } 
+
+    else { 
+        return "Jogador 2 venceu!";
+    }
+}
+    const resultado = playRockPaperScissor("Pedra", "Tesoura");
+      console.log(resultado); 
